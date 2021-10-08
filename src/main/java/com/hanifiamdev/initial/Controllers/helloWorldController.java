07644d6ae0aa5414f1bc66375b260f567ad7bb33
hanifiamdev <hanifamrullahdev@gmail.com>
2021-10-08 20:12:01 +0700

    @PathVariable dan @RequestParam

--- src/main/java/com/hanifiamdev/initial/Controllers/helloWorldController.java
+++ src/main/java/com/hanifiamdev/initial/Controllers/helloWorldController.java
@@ -1,13 +1,22 @@
 package com.hanifiamdev.initial.Controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class helloWorldController {
 
     @GetMapping("/Helloworld")
-    public  String Helloworld(){
+    public String Helloworld() {
         return "Hello world!";
+    }
+
+    @GetMapping("/Helloworld/{name}")
+    public String HelloworldName(@PathVariable(value = "name") String name) {
+        return "hello " + name;
+    }
+
+    @PostMapping("/Helloworld")
+    public String HelloworldPostController(@RequestParam(name = "Username") String username) {
+        return "hello " + username;
     }
 }
