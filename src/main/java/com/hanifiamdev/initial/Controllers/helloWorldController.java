3897b68a5b4a0450d6ff27a502289644669c149a
hanifiamdev <hanifamrullahdev@gmail.com>
2021-10-16 07:05:09 +0700

    Learning GET, POST, PUT, DELETE with https://jsonplaceholder.typicode.com/

--- src/main/java/com/hanifiamdev/initial/Controllers/helloWorldController.java
+++ src/main/java/com/hanifiamdev/initial/Controllers/helloWorldController.java
@@ -1,14 +1,75 @@
 package com.hanifiamdev.initial.Controllers;
 
+import com.hanifiamdev.initial.Entity.Todo;
 import com.hanifiamdev.initial.Entity.Username;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 import javax.validation.Valid;
+import java.util.Arrays;
 
 @RestController
 public class helloWorldController {
 
-    @GetMapping("/Helloworld")
+    RestTemplate restTemplate = new RestTemplate();
+
+    @GetMapping("/todo/get")
+    public String getTodo() {
+        String result = restTemplate.getForObject("https://jsonplaceholder.typicode.com/todos", String.class);
+        return result;
+    }
+
+    @GetMapping("todo/post")
+    public String postTodo() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+
+        Todo newTodo = new Todo();
+        newTodo.setId((long) 1000);
+        newTodo.setTitle("Joe");
+        newTodo.setUserId((long) 9);
+        newTodo.setCompleted(false);
+
+        HttpEntity<Todo> entity = new HttpEntity<Todo> (newTodo, headers);
+
+        return restTemplate.exchange("https://jsonplaceholder.typicode.com/todos", HttpMethod.POST, entity, String.class).getBody();
+
+    }
+
+    @GetMapping("/todo/delete")
+    public String deleteTodo() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+
+        Todo newTodo = new Todo();
+        newTodo.setId((long) 1);
+
+        HttpEntity<Todo> entity = new HttpEntity<Todo> (newTodo, headers);
+
+        return restTemplate.exchange("https://jsonplaceholder.typicode.com/todos/" + newTodo.getId(), HttpMethod.DELETE, entity, String.class).getBody();
+
+    }
+
+    @GetMapping("/todo/put/{id}")
+    public String updateTodo(@PathVariable("id") String id) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+
+        Todo updateTodo = new Todo();
+        updateTodo.setTitle("John Wick");
+        updateTodo.setCompleted(true);
+
+        HttpEntity<Todo> entity = new HttpEntity<Todo>(updateTodo, headers);
+
+        return restTemplate.exchange("https://jsonplaceholder.typicode.com/todos/" + id, HttpMethod.PUT, entity, String.class).getBody();
+
+    }
+
+   /* @GetMapping("/Helloworld")
     public String Helloworld() {
         return "Hello world!";
     }
@@ -26,5 +87,6 @@ public class helloWorldController {
     @PostMapping("/HelloworldUser")
     public Username HelloWordUserPostController(@Valid @RequestBody Username username) {
         return username;
-    }
+    } */
+
 }
---
+++ src/main/java/com/hanifiamdev/initial/Entity/Todo.java
@@ -0,0 +1,51 @@
+package com.hanifiamdev.initial.Entity;
+
+public class Todo {
+
+    private Long userId;
+    private Long id;
+    private String Title;
+    private Boolean completed;
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return Title;
+    }
+
+    public void setTitle(String title) {
+        Title = title;
+    }
+
+    public Boolean getCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(Boolean completed) {
+        this.completed = completed;
+    }
+
+    @Override
+    public String toString() {
+        return "Todo{" +
+                "userId=" + userId +
+                ", id=" + id +
+                ", Title='" + Title + '\'' +
+                ", completed=" + completed +
+                '}';
+    }
+}
